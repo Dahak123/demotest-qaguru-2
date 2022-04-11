57ee388bf5e66166cd7a61e89a266d528d3f19b5
Рустам <galeev560006@mail.ru>
2022-04-11 15:24:01 +0500

    adde junit fixtures

---
+++ src/test/java/quru/qa/FirstTest.java
@@ -0,0 +1,45 @@
+package quru.qa;
+
+import org.junit.jupiter.api.*;
+
+public class FirstTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("This is the before all method!");
+    }
+
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("This is the after all method!");
+    }
+
+    @BeforeEach
+    void openYandex() {
+        //open("ya.ru")
+        System.out.println("     open(\"ya.ru\")");
+    }
+
+    @AfterEach
+    void takeScreen() {
+        //takeScreen()
+        System.out.println("      takeScreen()");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("             first test()");
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("             second test()");
+        Assertions.assertTrue(true);
+    }
+}
+
+
+
+
